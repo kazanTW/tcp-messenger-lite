1d9499c5d160a630d58f69df87db2469965df668
Kazan <andyswebking@gmail.com>
2024-05-27 19:29:30 +0800

    Add README and comments on code

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # tcp-messenger-lite
 A Lite CLI messenger via TCP protocol in Python.
+
+## Requirement 
+- Python >= 3.11
+
+## Usage
+### Server
+```sh=
+python server.py --address <server_ip> --port <server_port>
+```
+
+### Client
+```sh=
+python client.py --ip <server_ip> --port <server_port>
+```
+- If you don't add any argument, the default IP address is `0.0.0.0`, port is `8080`.
+- `--address` can use `-a`, and `--port` can use `-p` instead.
--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import socket
 import threading
 
 
-def receive_messages(client_socket):
+def receive_messages(client_socket):                            # Define message receive on client
     while True:
         try:
             message = client_socket.recv(1024).decode('utf-8')
@@ -13,10 +13,11 @@ def receive_messages(client_socket):
         except:
             break
 
-def client(ip, port):
+def client(ip, port):                                           # Define a client
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((ip, port))
 
+    # Define receiving thread to let the program can work at the same time
     receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
     receive_thread.daemon = True
     receive_thread.start()
@@ -24,18 +25,18 @@ def client(ip, port):
     try:
         while True:
             try:
-                message = input()
+                message = input()                               # Get input
                 client_socket.send(message.encode('utf-8'))
-            except EOFError:
+            except EOFError:                                    # Use EOF(Ctrl-D/Z) to termainte
                 print("\nClient exit.")
                 break
-    except KeyboardInterrupt:
+    except KeyboardInterrupt:                                   # Use Ctrl-C to terminate
         print("\nClient exit.")
 
     client_socket.close()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="TCP P2P Message Client")
+    parser = argparse.ArgumentParser(description="TCP P2P Message Client")  # Using argparse library to let program can get arguments in command line
     parser.add_argument('-a', '--address', type=str, default='0.0.0.0', help='IP address to bind to')
     parser.add_argument('-p', '--port', type=int, default=8080, help='Port to bind to')
     args = parser.parse_args()
