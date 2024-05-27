01657e55809899e15ece148e1fe0a4ddd0e31a80
Kazan <andyswebking@gmail.com>
2024-05-27 18:20:06 +0800

    Let server can send messages, and terminate program with EOF

--- client.py
+++ client.py
@@ -1,17 +1,43 @@
+import argparse
 import socket
+import threading
 
-if __name__ == '__main__':
-    # name = input('Input Name: ')
-    host = input('Input link host: ')
-    port = 8080
 
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((host, port))
+def receive_messages(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            if not message:
+                break
+            print(f": {message}")
+        except:
+            break
+
+def client(ip, port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((ip, port))
+
+    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+    receive_thread.daemon = True
+    receive_thread.start()
 
     try:
-        client.send(b'GET / HTTP/1.1\r\nHost: 127.0.0.1\r\n\r\n')
-        response = client.recv(4096)
-        print(response.decode())
+        while True:
+            try:
+                message = input()
+                client_socket.send(message.encode('utf-8'))
+            except EOFError:
+                print("\nClient exit.")
+                break
+    except KeyboardInterrupt:
+        print("\nClient exit.")
 
-    except EOFError:
-        client.close()
+    client_socket.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="TCP P2P Message Client")
+    parser.add_argument('-a', '--address', type=str, default='0.0.0.0', help='IP address to bind to')
+    parser.add_argument('-p', '--port', type=int, default=8080, help='Port to bind to')
+    args = parser.parse_args()
+
+    client(args.address, args.port)
--- server.py
+++ server.py
@@ -1,29 +1,55 @@
+import argparse
 import socket
 import threading
 
-ip = '127.0.0.1'
-port = 8080
+def handle_client(client_socket):
+    def receive_messages():
+        while True:
+            try:
+                message = client_socket.recv(1024).decode('utf-8')
+                if not message:
+                    break
+                print(f": {message}")
+            except:
+                break
+    
+    receive_thread = threading.Thread(target=receive_messages)
+    receive_thread.daemon = True
+    receive_thread.start()
 
+    try:
+        while True:
+            try:
+                message = input()
+                client_socket.send(message.encode('utf-8'))
+            except EOFError:
+                print("\n Server shutting down.")
+                break
+    except KeyboardInterrupt:
+        print("\n Server shutting down.")
 
-def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((ip, port))
-    server.listen(5)
-    print(f'[*] Listening on {ip}:{port}')
+    client_socket.close()
+
+def server(ip, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((ip, port))
+    server_socket.listen(5)
+    print(f"Server listening on {ip}:{port}")
 
     while True:
-        client, address = server.accept()
-        print(f'[*] Accepted connection from {address[0]}:{address[1]}')
-        client_handler = threading.Thread(target=handle_client, args=(client,))
-        client_handler.start()
-
-
-def handle_client(client_socket):
-    with client_socket as sock:
-        request = sock.recv(1024)
-        print(f'[*] Received: {request.decode("utf-8")}')
-        sock.send(b'ACK')
-
+        try:
+            client_socket, address = server_socket.accept()
+            print(f"Accepted connection from {address}")
+            handle_client(client_socket)
+        except (EOFError, KeyboardInterrupt):
+            print("\n Server shutting down.")
+            server_socket.close()
+            break
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="TCP P2P Message Server")
+    parser.add_argument('-a', '--address', type=str, default='0.0.0.0', help='IP address to bind to')
+    parser.add_argument('-p', '--port', type=int, default=8080, help='Port to bind to')
+    args = parser.parse_args()
+
+    server(args.address, args.port)
